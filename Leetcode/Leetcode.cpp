afb1a5ef28485c4f973f3056883b7a4827110c73
Anton <golan349@gmail.com>
2021-11-07 21:52:39 +0500

    Multiple strings Leetcode

--- Leetcode/Leetcode.cpp
+++ Leetcode/Leetcode.cpp
@@ -1,34 +1,50 @@
 #include <iostream>
+#include <cstdio>
+#include <cstdlib>
 #include <string>
-#include <unordered_map>
+#include <bitset>
+#include <map>
+#include <set>
+#include <stack>
+#include <queue>
 #include <vector>
+#include <cmath>
+#include <algorithm>
+#include <unordered_map>
 
 using namespace std;
 
-int main()
-{
-	int val;
-	cin >> val;
-	vector<string> str(val);
-	unordered_map<char, int> str_stat;
-	for (int i = 0; i < val; i++)
-		cin >> str[i];
-	for (auto tmp : str)
-	{
-		if (tmp.size() % 2)
-		{
-			cout << "No\n";
-			continue;
+class Solution {
+public:
+	string multiply(string num1, string num2) {
+		if (num1 == "0" || num2 == "0") return 0;
+		int sz1(num1.length()), sz2(num2.length()), sz(sz1 + sz2);
+		vector<int> val(sz, 0);
+		string res(sz, '0');
+		for (int i = 0; i < sz1; i++) {
+			for (int j = 0; j < sz2; j++) {
+				val[i + j + 1] += (num1[i] - '0') * (num2[j] - '0');
+			}
 		}
-			
-		for (auto ch :tmp)
-		{
-			str_stat[ch]++;
+		for (int i = sz - 1; i >= 0; i--) {
+			if (val[i] > 9) {
+				val[i - 1] += val[i] / 10;
+				val[i] %= 10;
+			}
+			res[i] = val[i] + '0';
 		}
-		if (str_stat['A'] + str_stat['C'] == str_stat['B'])
-			cout << "Yes\n";
-		else
-			cout << "No\n";
-		str_stat.clear();
+		if (res[0] == '0') res.erase(res.begin());
+		return res;
 	}
+};
+
+
+
+int main() {
+	
+	string str1("101");
+	string str2("657");
+	Solution sol;
+	sol.multiply(str1, str2);
+	return 0;
 }
